3f936f7aec0f548ad5489458f6fecd4f2a8d67a5
felipeassis97 <felipeassis97@gmail.com>
2024-06-11 08:27:09 +1000

    feat: loan view development

--- Classes/LoanView.swift
+++ Classes/LoanView.swift
@@ -16,8 +16,8 @@ public struct LoanView: View {
                                         blue: 240.0/255.0)
     
     private let yellowColor = Color(red: 255.0/255.0,
-                                        green: 186.0/255.0,
-                                        blue: 5.0/255.0)
+                                    green: 186.0/255.0,
+                                    blue: 5.0/255.0)
     
     public var body: some View {
         ZStack(alignment: .top) {
@@ -26,6 +26,8 @@ public struct LoanView: View {
             VStack{
                 headerView
                     .padding(.leading, 20)
+                cardView
+                    .padding()
             }
         }
     }
@@ -55,8 +57,61 @@ public struct LoanView: View {
                 .fontWeight(.light)
         }
     }
+    
+    var cardView: some View {
+        RoundedRectangle(cornerRadius: 7)
+            .frame(maxWidth: .infinity)
+            .frame(height: 140)
+            .foregroundStyle(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 7.0))
+            .shadow(color: .green.opacity(0.5), radius: 7, x: 0, y:2)
+            .overlay {
+                VStack(alignment: .leading) {
+                    headerCardView
+                        .padding(.top, 20)
+                        .padding(.horizontal)
+                    
+                    headerTextCardView
+                        .padding(.horizontal)
+                    
+                    Spacer()
+                }
+            }
+    }
+    
+    var headerCardView: some View {
+        VStack(alignment: .leading){
+            Text("Veja sua opção de crédito pré aprovado")
+                .font(.system(size: 16))
+                .foregroundStyle(.black)
+                .fontWeight(.medium)
+                .padding(.bottom, 5)
+            
+            HStack {
+                Image(systemName: "dolarsign.arrow.circlepath")
+                    .font(.system(size: 22))
+                
+                Text("Empréstimo pessoal")
+                    .font(.system(size: 16))
+                    .foregroundStyle(.black)
+                    .fontWeight(.light)
+                
+                Spacer()
+            }
+            .padding(.bottom, 5)
+        }
+    }
+    
+    var headerTextCardView: some View {
+        HStack {
+            Text("Guardar dinheiro é o primeiro passo pra realizar grandes sonhos")
+                .font(.system(size: 12))
+                .foregroundStyle(.black)
+                .fontWeight(.light)
+            
+        }
+    }
 }
-
 #Preview {
     LoanView()
 }
